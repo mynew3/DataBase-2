7ce877d8d628087a3cb3324ef24b3dfc47668f30
zgn <zgn88@mail.ru>
2017-02-11 18:56:37 +0300

    DB/Trainer: some correct npc flag

---
+++ Updates/2017_02_11_01_pl.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_template` SET `npcflag` = 211  WHERE `entry` = 4941;
+UPDATE `creature_template` SET `npcflag` = 81  WHERE `entry` IN (7232, 8738); 
+UPDATE `creature_template` SET `npcflag` = 209 WHERE `entry` IN (18749, 18752, 18772, 44975); 
+UPDATE `creature_template` SET `npcflag` = 4241 WHERE `entry` IN (45286, 57620); 
+UPDATE `creature_template` SET `npcflag` = 115 WHERE `entry` = 50247;
